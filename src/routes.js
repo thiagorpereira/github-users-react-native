1a3a380219eb7ea2a68d5f76731c47f79345dc0b
Thiago Pereira <thiagorpereira7@gmail.com>
2020-02-07 14:03:55 -0200

    Insert Stack Navigation and Main page with styles

--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,21 @@ import Main from './pages/Main';
 import User from './pages/User';
 
 const Routes = createAppContainer(
-  createStackNavigator({
-    Main,
-    User,
-  })
+  createStackNavigator(
+    {
+      Main,
+      User,
+    },
+    {
+      defaultNavigationOptions: {
+        headerTitleAlign: 'center',
+        headerBackTitleVisible: false,
+        headerStyle: {
+          backgroundColor: '#7159c1',
+        },
+        headerTintColor: '#FFF',
+      },
+    }
+  )
 );
 export default Routes;
